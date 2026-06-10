393e4eb88a1845537d29fe49633f94d0352ac9d6
Ben Spiers <ben.spiers22@gmail.com>
2026-06-10 16:25:18 +0100

    Make some integrators more robust to axial rotations

--- src/integrators.rs
+++ src/integrators.rs
@@ -45,13 +45,24 @@ pub fn update_proposed(q: Quat, J: Mat3, world_momentum: Vec3, dt: f32) -> (Quat
     let body_l0 = q.inverse() * world_momentum;
     let body_w = J * body_l0;
     let k = body_w.cross(body_l0);
-    let j_k = k.dot(J * k) / k.length_squared();
-    let omega = body_w - j_k * body_l0;
-    let omega_world = q * omega;
-    let q_omega = Quat::from_scaled_axis(omega_world * dt);
-    let q_l = Quat::from_scaled_axis(j_k * world_momentum * dt);
+    let k2 = k.length_squared();
 
-    let q1 = q_l * q_omega * q;
+    let q_step = if k2 > 1e-8 {
+        let j_k = k.dot(J * k) / k2;
+        let omega = body_w - j_k * body_l0;
+        let omega_world = q * omega;
+        let q_omega = Quat::from_scaled_axis(omega_world * dt);
+        let q_l = Quat::from_scaled_axis(j_k * world_momentum * dt);
+
+        q_l * q_omega
+    } else {
+        let omega_world = q * body_w;
+        let q_omega = Quat::from_scaled_axis(omega_world * dt);
+
+        q_omega
+    };
+
+    let q1 = q_step * q;
 
     (q1, world_momentum)
 }
@@ -60,19 +71,22 @@ pub fn update_proposed_velocity(q: Quat, J: Mat3, world_momentum: Vec3, dt: f32)
     let body_l0 = q.inverse() * world_momentum;
     let body_w = J * body_l0;
     let k = body_w.cross(body_l0);
-    let j_k = k.dot(J * k) / k.length_squared();
-    let omega = body_w - j_k * body_l0;
 
-    let q_omega = Quat::from_scaled_axis(-omega * dt);
+    let k2 = k.length_squared();
 
-    let body_l1 = q_omega * body_l0;
+    let body_l1 = if k2 > 1e-8 {
+        let j_k = k.dot(J * k) / k2;
+        let omega = body_w - j_k * body_l0;
+        let q_omega = Quat::from_scaled_axis(-omega * dt);
+        q_omega * body_l0
+    } else {
+        body_l0
+    };
+
     let body_w1 = J * body_l1;
-
     let world_l1 = q * body_l1;
     let world_w1 = q * body_w1;
-
     let q_step = Quat::from_scaled_axis(world_w1 * dt);
-
     let q1 = q_step * q;
     let world_l_out = q_step * world_l1;
 
